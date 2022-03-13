c17978f3c9a56fa059ab03005f211dc7f0281d63
Matt Fors <mfors1@binghamton.edu>
2022-03-13 14:36:35 -0400

    Start laying out tab component

---
+++ projects/ngx-browser-tabs-showcase/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NgxBrowserTabsModule } from '../../../ngx-browser-tabs/src/lib/ngx-browser-tabs.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgxBrowserTabsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
